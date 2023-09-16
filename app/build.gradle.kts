42875cdbba03c816bb59179460f38a8861805fd3
ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>
2023-09-17 00:50:01 +0300

    Api runs successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -49,7 +52,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation ("com.github.bumptech.glide:glide:4.13.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-alpha02")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
 
 }
--- app/src/main/java/com/example/mymeals/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mymeals/presentation/ui/activities/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.mymeals.presentation.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
 import com.example.mymeals.R
+import com.example.mymeals.databinding.ActivityMainBinding
+import com.example.mymeals.presentation.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewMvvm: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize your MainViewModel using ViewModelProvider
+        mainViewMvvm = ViewModelProvider(this)[MainViewModel::class.java]
+
+        mainViewMvvm.getTenRandomMeals()
+        observeRandomMeal()
+    }
+
+    private fun observeRandomMeal() {
+        mainViewMvvm.observeTenRandomMealsLiveData().observe(this) { randomMeals ->
+            Log.d("Main activity random meals", randomMeals.toString())
+        }
     }
 }
